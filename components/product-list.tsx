2616c6ef494fb384dec41987d1b47dd95d580ee4
technologystream9840 <technologystream9840@gmail.com>
2025-05-27 15:04:08 +0000

    chore:fix file upload

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -44,7 +44,7 @@ const ProductList: React.FC = () => {
               </div>
               <div className="p-4">
                 <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-                <p className="text-gray-700">${product.price.toFixed(2)}</p>
+                <p className="text-gray-700">${product.price}</p>
               </div>
             </li>
           ))}
