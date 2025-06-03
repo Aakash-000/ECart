1f02fae9512850c9bcf3887ef6716684fc5fba49
technologystream9840 <technologystream9840@gmail.com>
2025-06-03 12:48:46 +0000

    chore:correct query function

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -50,8 +50,8 @@ export default function ProductDetailPage({ params }) {
     console.log(res)
     const data = await res.json();
     return data;
-  };
-  
+  };(
+  console.log(isError,error)
   console.log(product)
   const handleAddToCart = () => {
     if (!product) return;
